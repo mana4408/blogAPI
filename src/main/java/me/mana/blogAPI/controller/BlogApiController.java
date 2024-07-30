1f69e7513768a44ca5a2380f541ac9efac5a6e27
mana <bs03127@naver.com>
2024-07-30 16:15:18 +0900

    Feat: 추가 글 조회 기능
    id별 블로그 글 조회하는 기능 개발
    findArticle() 테스트 메서드 개발

--- src/main/java/me/mana/blogAPI/controller/BlogApiController.java
+++ src/main/java/me/mana/blogAPI/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import me.mana.blogAPI.dto.ArticleResponse;
 import me.mana.blogAPI.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +35,13 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable Long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
